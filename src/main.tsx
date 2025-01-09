c85a9da870bb6e3570344b2cf2305c76108a1de9
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-01-09 17:25:42 -0300

    ✨ feat: adiciona suporte a notificações com Chakra UI Toast

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { createStandaloneToast } from '@chakra-ui/toast'
 import AppRoutes from '@/routes'
 import store from '@/store'
 import '@/sass/_global.scss'
 
+const { ToastContainer } = createStandaloneToast()
+
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <Provider store={store}>
+            <ToastContainer />
             <AppRoutes />
         </Provider>
     </StrictMode>
